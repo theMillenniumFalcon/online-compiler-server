8ffdfa76aa73392b1397b4dd0e101becda83bca8
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-09-15 00:43:33 +0530

    cleaned a bit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import dotenv from "dotenv"
 
 dotenv.config()
-const PORT = process.env.SERVER_PORT
+const PORT = process.env.PORT
 import app from "./app";
 
 app.listen(PORT, () => {
-    // tslint:disable-next-line
     console.log("Express server listening on port " + PORT)
 })
